f483696638429a62f5cef92efd0bbadcdd2339fe
jamaleddynimad <i.jamaleddyn@gmail.com>
2019-03-14 21:27:26 +0100

    new update

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ Created on Sun Mar 10 23:50:42 2019
 import numpy as np
 import re
 import pickle 
-import nltk
+import nltk as nl
 from nltk.corpus import stopwords
 from sklearn.datasets import load_files
 nltk.download('stopwords')
